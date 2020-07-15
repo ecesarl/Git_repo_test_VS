8760753b31bbb3a229e550825fb32ecad30d052d
Eugenio <eucelor@yahoo.com>
2020-07-15 09:41:28 -0700

    Initializig the project

---
+++ Git-repo-test_VS/Git-repo-test_VS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Git_repo_test_VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mi primer test con Git");
+        }
+    }
+}
